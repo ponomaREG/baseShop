47f32f4f604acae4436d4c0e8723f8c7c9a84312
disfunction <ponomarcomru@gmail.com>
2020-04-18 15:37:34 +0600

    init // main_bnv

---
+++ app/src/main/java/com/test/baseshop/baseActivityWithFragments.java
@@ -0,0 +1,14 @@
+package com.test.baseshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class baseActivityWithFragments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
